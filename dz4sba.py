f3cbac9af2afd73723fcf32c3ce18545014a00fd
Bog Gus <bog.sosnin@gmail.com>
2024-12-13 01:06:19 +0300

    new

--- dz4sba.py
+++ dz4sba.py
@@ -1,1 +1,7 @@
+import json
 
+purchases={}
+
+with open(file='purchase_log.txt') as file:
+    for line in file:
+        purchases[json.loads(line).get("user_id")] = json.loads(line).get("category")
