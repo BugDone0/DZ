d0d41459e8b706dc85994346a863c42c0045968d
Bog Gus <bog.sosnin@gmail.com>
2024-12-15 03:13:09 +0300

    update

--- dz41sba.py
+++ dz41sba.py
@@ -1,1 +1,19 @@
+import json
 
+purchases={}
+
+with open(file='purchase_log.txt') as file:
+    for line in file:
+        purchases[json.loads(line).get("user_id")] = json.loads(line).get("category")
+
+f_funnel = open('funnel.csv', 'w')
+visit_log = open('visit_log.csv', 'r')
+for line in visit_log:
+    user_inf=line.split(',')
+    try:
+        f_funnel.write(f"{user_inf[0]}, {user_inf[1].strip()}, {purchases[user_inf[0]]}\n")
+    finally:
+        continue
+
+visit_log.close()
+f_funnel.close()
